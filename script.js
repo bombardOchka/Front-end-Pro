bc3e5fa2022acd02522f2383bd37b5ab8f5105d6
bombardOchka <fvvcvjfbhhf@gmail.com>
2023-06-30 20:17:57 +0300

    new dz

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-const divContain = document.body.children[0].children;
-
-const text = document.getElementById("text");
-const buttonShow = document.getElementById("show-hide");
-const buttonBigger = document.getElementById("bigger");
-const buttonSmaller = document.getElementById("smaller");
-
-let counter = 0;
-let size = 100;
+const titleInput = document.getElementById('title');
+const descriptionInput = document.getElementById('description');
+const propertySelect = document.getElementById('property');
+const addButton = document.getElementById('add');
+const todoList = document.getElementById('List');
 
 
-buttonShow.onclick = function() {
-    counter += 1;
-    for(let i = 0; i < divContain.length; i++) {
-        if (divContain[i].id !== 'show-hide') {
-        divContain[i].classList.toggle('visible');
-        }
-        else {
-            if (counter % 2 === 1) {
-                divContain[i].innerText = 'Show text';
-                
-            }
-            else {
-                divContain[i].innerText = 'Hide text';
-            }
-        }
-    }
-}
+
+addButton.addEventListener('click', function() {
+  if (titleInput.value.trim() !== '') {
+  const todoItem = document.createElement('li');
+  const property = propertySelect.value;
+  
+  if (property === 'low') {
+    todoItem.classList.add('low');
+  } else if (property === 'mid') {
+    todoItem.classList.add('mid');
+  } else if (property === 'high') {
+    todoItem.classList.add('high');
+  }
+
+  const titleElement = document.createElement('h3');
+  const descriptionElement = document.createElement('p');
+  const checkbox = document.createElement('input');
+  const deleteButton = document.createElement('button');
+
+  checkbox.type = 'checkbox';
+  titleElement.innerText = titleInput.value;
+  descriptionElement.innerText = descriptionInput.value;
+  deleteButton.innerText = 'delete';
+
+  checkbox.addEventListener('click', function() {
+    titleElement.classList.toggle('lineThrough');
+  });
 
 
-buttonBigger.onclick = function() {
-    size += 1;
-    text.style.fontSize = size + 'px';
-}
+  deleteButton.addEventListener('click', function() {
+    if (titleElement.classList.contains('lineThrough')) {
+    todoItem.remove();
+    }});
 
-buttonSmaller.onclick = function() {
-    size -= 1;
-    text.style.fontSize = size + 'px';
-}
+
+  todoItem.append(titleElement);
+  todoItem.append(checkbox);
+  todoItem.append(descriptionElement);
+  todoItem.append(deleteButton);
+ 
+  
+  todoList.append(todoItem);
+
+  titleInput.value = '';
+  descriptionInput.value = '';
+  }
+});
 
